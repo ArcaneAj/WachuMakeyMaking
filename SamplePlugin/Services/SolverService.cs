6ac2214bcb6d8f2567c8eaa115703e99723944be
ArcaneAj <41121914+ArcaneAj@users.noreply.github.com>
2026-01-20 18:36:00 +1300

    Fully removed sonnet references

--- SamplePlugin/Services/SolverService.cs
+++ SamplePlugin/Services/SolverService.cs
@@ -1,10 +1,7 @@
 using Lumina.Excel.Sheets;
 using SamplePlugin.Models;
-using Sonnet;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.PortableExecutable;
-using System.Xml.Linq;
 
 namespace SamplePlugin.Services
 {
@@ -40,10 +37,8 @@ namespace SamplePlugin.Services
             var resources = crystals.Concat(items).ToArray();
             var recipes = selectedRecipes.Select(x => new RecipeAssignment(x.Value, x.Item, x.Ingredients));
 
-            var model = new Model();
-
-            model.Name = "RecipeChoice";
-            model.Objective = recipes.Sum(a => a.Cost * a.Assign);
+            //model.Name = "RecipeChoice";
+            //model.Objective = recipes.Sum(a => a.Cost * a.Assign);
 
             foreach (var resource in resources)
             {
@@ -53,7 +48,7 @@ namespace SamplePlugin.Services
                 {
                     Plugin.Log.Info($"    {rur.Item.Name}");
                 }
-                model.Add($"Availability[{resource.Item.Name}]", recipesUsingResource.Sum(a => a.Ingredients[resource.Item] * a.Assign) <= resource.Quantity);
+                //model.Add($"Availability[{resource.Item.Name}]", recipesUsingResource.Sum(a => a.Ingredients[resource.Item] * a.Assign) <= resource.Quantity);
             }
         }
     }
@@ -63,6 +58,5 @@ namespace SamplePlugin.Services
         public double Cost { get; private set; } = cost;
         public Item Item { get; } = item;
         public Dictionary<Item, byte> Ingredients { get; } = ingredients;
-        public Variable Assign { get; private set; } = default!;
     }
 }
